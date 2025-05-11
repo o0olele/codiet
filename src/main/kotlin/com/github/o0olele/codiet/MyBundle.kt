4efabcf333c585e9999ff84c66a61251bfef77d0
GitHub Action <action@github.com>
2025-05-11 06:17:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/o0olele/codiet/MyBundle.kt
+++ src/main/kotlin/com/github/o0olele/codiet/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.o0olele.codiet
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/o0olele/codiet/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/o0olele/codiet/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.o0olele.codiet.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
