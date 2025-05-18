bcd14fb5e34ebfd81ad78ce990e6676c256970b5
o0olele <lr18516312@163.com>
2025-05-18 23:08:16 +0800

    [gradle] add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ repositories {
 dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
+    implementation("io.github.ollama4j:ollama4j:1.0.100")
+    implementation("com.openai:openai-java:1.6.1")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
